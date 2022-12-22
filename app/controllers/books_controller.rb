4ceecf863aab3a491f3015e534ebf762c26aa1c3
AaronMorimoto <119596908+AaronMorimoto@users.noreply.github.com>
2022-12-22 13:31:20 +0000

    [Add 1222]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -37,7 +37,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @book.user_id = current_user.id
-    if @book.update(user_params)
+    if @book.update(book_params)
       flash[:notice] = "You have updated book successfully."
       redirect_to book_path(@book.id)
     else
